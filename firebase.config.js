01ecfa310f8472aa6a31ba1c9ff1d938ef098ebd
Jenna <jenna48071@gmail.com>
2023-01-30 20:00:13 -0600

    installed firebase and tailwind

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbVKMayIDQOPoVRILn2qzpHmxb4YKfaSo",
+  authDomain: "market-c4fb2.firebaseapp.com",
+  projectId: "market-c4fb2",
+  storageBucket: "market-c4fb2.appspot.com",
+  messagingSenderId: "825151953558",
+  appId: "1:825151953558:web:d5ec1d0bc80a373ee480b7",
+};
+
+initializeApp(firebaseConfig);
