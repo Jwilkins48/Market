5436803b388d2367967045a90ea8435d28240440
Jenna <jenna48071@gmail.com>
2023-02-06 15:50:02 -0600

    styled sign up form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -62,10 +62,10 @@ function SignUp() {
   };
 
   return (
-    <div className="bg-gray-300 flex h-[92.4vh] justify-center items-center">
+    <div className="flex h-[92.4vh] justify-center items-center">
       <form
         onSubmit={onSubmit}
-        className="w-[23rem] bg-primary h-[32rem] rounded-lg border border-base-300 shadow-xl p-3 m-2 bg- flex flex-col"
+        className="w-[23rem] bg-primary h-[27rem] rounded-lg border border-base-300 shadow-xl p-3 m-2 bg- flex flex-col"
       >
         {/* SIGN IN / SIGN UP */}
         <header className="grid grid-cols-3 my-2 justify-center items-center text-center signIn">
@@ -78,13 +78,11 @@ function SignUp() {
           <h1 className="font-bold text-yellow-200 ml-2 text-xl">Sign up</h1>
         </header>
         {/* USER NAME */}
-        <div className="flex flex-col my-20 gap-3 h-40 justify-around">
+        <div className="flex flex-col my-20 gap-3 h-24 justify-around">
           <div>
-            <label className="font-bold mr-2 text-secondary" htmlFor="name">
-              Name
-            </label>
             <input
-              className="input text-gray-600 bg-base-100 border border-gray-300 h-10 w-full mt-1 text-secondary "
+              placeholder="Name"
+              className="input text-gray-600 bg-base-100 border border-gray-300 w-full mt-1 text-secondary "
               onChange={onChange}
               type="text"
               id="name"
@@ -93,11 +91,9 @@ function SignUp() {
           </div>
           {/* EMAIL*/}
           <div>
-            <label className="font-bold mr-2 text-secondary" htmlFor="email">
-              Email
-            </label>
             <input
-              className="input text-gray-600 bg-base-100 border border-gray-300 h-10 w-full mt-1 text-secondary "
+              placeholder="Email"
+              className="input text-gray-600 bg-base-100 border border-gray-300  w-full mt-1 text-secondary "
               onChange={onChange}
               type="text"
               id="email"
@@ -106,12 +102,10 @@ function SignUp() {
           </div>
           {/* PASSWORD */}
           <div className="flex flex-col">
-            <label className="font-bold text-secondary mr-2" htmlFor="password">
-              Password
-            </label>
             <div className="relative">
               <input
-                className="input text-gray-600 bg-base-100 border border-gray-300 h-10 w-full mt-1 text-secondary"
+                placeholder="Password"
+                className="input text-gray-600 bg-base-100 border border-gray-300  w-full mt-1 text-secondary"
                 onChange={onChange}
                 type={showPassword ? "text" : "password"}
                 id="password"
@@ -120,30 +114,25 @@ function SignUp() {
               {/* SHOW PASSWORD */}
               <p onClick={() => setShowPassword(!showPassword)}>
                 {!showPassword ? (
-                  <i className="fa-regular text-gray-600 fa-eye absolute top-4 right-5 text-secondary" />
+                  <i className="fa-regular text-gray-600 fa-eye absolute top-5 right-5 text-secondary" />
                 ) : (
-                  <i className="fa-solid text-gray-600 fa-eye-slash absolute top-4 right-5 text-secondary" />
+                  <i className="fa-solid text-gray-600 fa-eye-slash absolute top-5 right-5 text-secondary" />
                 )}
               </p>
             </div>
             {/* CONFIRM PASSWORD */}
-            <label
-              className="font-bold text-secondary mr-2 mt-4"
-              htmlFor="confirmPassword"
-            >
-              Confirm Password
-            </label>
             <input
+              placeholder="Confirm Password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               id="confirmPassword"
-              className="input text-gray-600 bg-base-100 border border-gray-300 h-10 w-full mt-1 text-secondary"
+              className="input text-gray-600 bg-base-100 border border-gray-300 w-full mt-4 text-secondary"
               type={showPassword ? "text" : "password"}
             />
           </div>
         </div>
         {/* SUBMIT */}
-        <button className="btn bg-yellow-200 text-gray-500 border-0 mt-8">
+        <button className="btn bg-yellow-200 text-gray-500 border-0 mt-4">
           Sign Up
         </button>
       </form>
